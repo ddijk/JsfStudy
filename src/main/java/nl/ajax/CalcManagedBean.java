e511567e880418c5dbfa8276fc628914cc4622b1
Dick Dijk <dick.dijk@ortec-finance.com>
2015-09-15 08:01:18 +0200

    add selectOneMenu control

--- src/main/java/nl/ajax/CalcManagedBean.java
+++ src/main/java/nl/ajax/CalcManagedBean.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package nl.ajax;
 
-import javax.enterprise.context.Dependent;
+import java.util.Arrays;
+import java.util.List;
 import javax.enterprise.inject.Model;
 import javax.inject.Named;
 
@@ -20,8 +20,12 @@ public class CalcManagedBean {
 
     int getal1;
     int getal2;
-    
+
     int result;
+
+    List<String> colors = Arrays.asList("blue", "red", "yellow");
+    String selectedColor;
+
     /**
      * Creates a new instance of CalcManagedBean
      */
@@ -45,21 +49,43 @@ public class CalcManagedBean {
     }
 
     public int getResult() {
-        System.out.println("getal1 ="+ getal1+", getal2="+ getal2);
+        System.out.println("getal1 =" + getal1 + ", getal2=" + getal2);
         return result;
     }
 
     public void setResult(int result) {
-         System.out.println("getal1 ="+ getal1+", getal2="+ getal2);
+        System.out.println("getal1 =" + getal1 + ", getal2=" + getal2);
         this.result = result;
     }
-    
+
     public void calculate() {
-         System.out.println("getal1 ="+ getal1+", getal2="+ getal2);
+        System.out.println("getal1 =" + getal1 + ", getal2=" + getal2);
         result = getal1 + getal2;
     }
-    
-    
-    
-    
+
+    public void method1() {
+        System.out.println("method1 called, color =" + selectedColor);
+    }
+
+    public void method2() {
+        System.out.println("method2 called");
+    }
+
+    public List<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(List<String> colors) {
+        this.colors = colors;
+    }
+
+    public String getSelectedColor() {
+        return selectedColor;
+    }
+
+    public void setSelectedColor(String selectedColor) {
+        System.out.println("color set to " + selectedColor);
+        this.selectedColor = selectedColor;
+    }
+
 }

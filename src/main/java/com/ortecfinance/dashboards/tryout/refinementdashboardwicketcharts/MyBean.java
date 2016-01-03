d34875e458e5ecb1c68f85aebec17db1065846a4
ddijk <dick@dijkrosoft.nl>
2016-01-03 19:33:08 +0100

    fix highcharts

--- src/main/java/com/ortecfinance/dashboards/tryout/refinementdashboardwicketcharts/MyBean.java
+++ src/main/java/com/ortecfinance/dashboards/tryout/refinementdashboardwicketcharts/MyBean.java
@@ -14,68 +14,81 @@ import java.util.Arrays;
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import org.apache.log4j.Logger;
 
 @Model
 public class MyBean {
 
-    Options options;
+	private static final Logger LOGGER = Logger.getLogger(MyBean.class);
 
-    @Inject
-    HelloBean helloBean;
+	Options options;
 
-    @PostConstruct
-    public void init() {
-        options = new Options();
+	@Inject
+	HelloBean helloBean;
 
-        options
-                .setChartOptions(new ChartOptions()
-                        .setType(SeriesType.LINE));
+	String title;
 
-        options
-                .setTitle(new Title("My very own chart " + helloBean.getName()));
+	@PostConstruct
+	public void init() {
+		LOGGER.info("*** init ***");
+		options = new Options();
 
-        options
-                .setxAxis(new Axis()
-                        .setCategories(Arrays
-                                .asList(new String[]{"Jan", "Feb", "Mar", "Apr", "May",
-                                    "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"})));
+		options
+			.setChartOptions(new ChartOptions()
+				.setType(SeriesType.LINE));
 
-        options
-                .setyAxis(new Axis()
-                        .setTitle(new Title("Temperature (C)")));
+		options
+			.setxAxis(new Axis()
+				.setCategories(Arrays
+					.asList(new String[]{"Jan", "Feb", "Mar", "Apr", "May",
+						"Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"})));
 
-        options
-                .setLegend(new Legend()
-                        .setLayout(LegendLayout.VERTICAL)
-                        .setAlign(HorizontalAlignment.RIGHT)
-                        .setVerticalAlign(VerticalAlignment.TOP)
-                        .setX(-10)
-                        .setY(100)
-                        .setBorderWidth(0));
+		options
+			.setyAxis(new Axis()
+				.setTitle(new Title("Temperature (C)")));
 
-        options
-                .addSeries(new SimpleSeries()
-                        .setName("Tokyo")
-                        .setData(
-                                Arrays
-                                .asList(new Number[]{7.0, 6.9, 9.5, 14.5, 18.2, 21.5,
-                                    25.2, 26.5, 23.3, 18.3, 13.9, 9.6})));
+		options
+			.setLegend(new Legend()
+				.setLayout(LegendLayout.VERTICAL)
+				.setAlign(HorizontalAlignment.RIGHT)
+				.setVerticalAlign(VerticalAlignment.TOP)
+				.setX(-10)
+				.setY(100)
+				.setBorderWidth(0));
 
-        options
-                .addSeries(new SimpleSeries()
-                        .setName("New York")
-                        .setData(
-                                Arrays
-                                .asList(new Number[]{-0.2, 0.8, 5.7, 11.3, 17.0, 22.0,
-                                    24.8, 24.1, 20.1, 14.1, 8.6, 2.5})));
-    }
+		options
+			.addSeries(new SimpleSeries()
+				.setName("Tokyo")
+				.setData(
+					Arrays
+					.asList(new Number[]{7.0, 6.9, 9.5, 14.5, 18.2, 21.5,
+						25.2, 26.5, 23.3, 18.3, 13.9, 9.6})));
 
-    public Options getOptions() {
-        return options;
-    }
+		options
+			.addSeries(new SimpleSeries()
+				.setName("New York")
+				.setData(
+					Arrays
+					.asList(new Number[]{-0.2, 0.8, 5.7, 11.3, 17.0, 22.0,
+						24.8, 24.1, 20.1, 14.1, 8.6, 2.5})));
+	}
 
-    public void setOptions(Options options) {
-        this.options = options;
-    }
+	public Options getOptions() {
+		options.setTitle(new Title("My very own chart " + helloBean.getName() + "---" + title));
+		return options;
+	}
+
+	public void setOptions(Options options) {
+		this.options = options;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		LOGGER.info("Title set to " + title);
+		this.title = title;
+	}
 
 }
--- src/main/java/nl/MyBB.java
+++ src/main/java/nl/MyBB.java
@@ -8,6 +8,7 @@ package nl;
 import java.util.Date;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.inject.Named;
 import org.apache.log4j.Logger;
@@ -20,32 +21,34 @@ import org.apache.log4j.Logger;
 @RequestScoped
 public class MyBB {
 
-    private static final Logger LOGGER = Logger.getLogger(MyBB.class);
+	private static final Logger LOGGER = Logger.getLogger(MyBB.class);
 
-    MyActionListener myActionListener;
+	MyActionListener myActionListener;
 
-    @PostConstruct
-    public void init() {
-        System.out.println("Constr MyBB");
-        myActionListener = new MyActionListener();
-    }
+	@PostConstruct
+	public void init() {
+		System.out.println("Constr MyBB");
+		myActionListener = new MyActionListener();
+	}
 
-    public MyActionListener getMyActionListener() {
-        return myActionListener;
-    }
+	public MyActionListener getMyActionListener() {
+		return myActionListener;
+	}
 
-    public void setMyActionListener(MyActionListener myActionListener) {
-        this.myActionListener = myActionListener;
+	public void setMyActionListener(MyActionListener myActionListener) {
+		this.myActionListener = myActionListener;
 
-        System.out.println("myBB " + myActionListener.getNaam() + " @ " + new Date());
-    }
+		System.out.println("myBB " + myActionListener.getNaam() + " @ " + new Date());
+	}
 
-    public void handleAfterPhaseEvent(PhaseEvent pe) {
-        LOGGER.info("handle after PhaseEvent " + pe.getPhaseId());
-    }
+	public void handleAfterPhaseEvent(PhaseEvent pe) {
+		LOGGER.info("handle after PhaseEvent " + pe.getPhaseId());
+	}
 
-    public void handleBeforePhaseEvent(PhaseEvent pe) {
-        LOGGER.info("handle before PhaseEvent " + pe.getPhaseId());
-    }
+	public void handleBeforePhaseEvent(PhaseEvent pe) {
+		String info = FacesContext.getCurrentInstance().getExternalContext().getRequestPathInfo();
+		LOGGER.info("pad info : " + info);
+		LOGGER.info("handle before PhaseEvent " + pe.getPhaseId());
+	}
 
 }
